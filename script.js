48ea7143b0683b873dc08d49f61b6de1008534ea
Vaishak-09 <vaishakvsaliyan@gmai.com>
2023-12-24 14:27:12 +0530

    time limit first commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let text="";
-let x=1;
+let x=0,y=0,s=0,ps=0,fs=0,sdif=0,flag=0;
 function load()
 {
+    flag=0;
     document.getElementById("lbtn").disabled=true;
     document.getElementById("rbtn").disabled=true;
     document.getElementById("lbtn").innerHTML="";
@@ -23,6 +24,10 @@ function start()
     
     else if(document.getElementById('startbtn').innerHTML=="Start")
     {
+        alert("Choose the correct color within 2 Seconds");
+
+        let d=new Date();
+        fs=d.getSeconds();
         document.getElementById("scorebtn").disabled=false;
         document.getElementById("lbtn").disabled=false;
         document.getElementById("rbtn").disabled=false;
@@ -52,18 +57,27 @@ function start()
 }
 function lclick()
 {   
+    y=checktime();
+    if(y==1)
+    {
     x=checkans("lbtn",text);
     if(x==1)
     {
      randomcolor();
     }
+    }
    
 }
 function rclick()
-{   x=checkans("rbtn",text);
+{ 
+    y=checktime();
+    if(y==1)
+    {
+    x=checkans("rbtn",text);
     if(x==1)
     {
       randomcolor();
+    }
     }
 }
 function randomcolor()
@@ -88,6 +102,44 @@ function randomcolor()
     const div1=division[Math.floor(Math.random()*division.length)];
     document.getElementById(div1).innerHTML=text;
     }
+}
+function checktime()
+{
+    let d=new Date();
+    s=d.getSeconds();
+    if(flag==0)
+    {
+        flag=1;
+        if(fs>s)
+        {
+            sdif=60-ps+s;
+        }
+        else
+        {
+            sdif=s-fs;
+        }
+    }
+    else if(ps>s)
+    {
+
+        sdif=60-ps+s;
+    }
+    else
+    {
+        sdif=s-ps;
+    }
+    ps=s;
+    if(sdif<=2)
+    {
+        return 1;
+    }
+    else
+    {
+        alert("time up");
+        score();
+        load();
+    }
+
 }
 function checkans(a,d)
 {  
